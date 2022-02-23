0157fd1eeb0a9e1ef1f39ef3ca78d6496705dc0d
al1yew <98798528+al1yew@users.noreply.github.com>
2022-02-23 13:59:21 -0800

    Add files via upload

---
+++ Task1_never_say_never/Task1_never_say_never/Models/Student.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1_never_say_never.Models
+{
+    class Student
+    {
+        private string _fullname;
+        public string FullName
+        {
+            get => _fullname;
+            set
+            {
+                while (!CheckFullname(value))  //checka gonderilen value false kimi qayidsa, true olur
+                {
+                    Console.WriteLine("Adinizi duz yazin.");
+                    value = Console.ReadLine();
+                }
+                _fullname = value;
+            }
+        }
+        public int Age { get; set; } // durub ozuycun
+
+        private string _groupno; // private get set etmirik
+        public string GroupNo // burdan qayidacag ora
+        {
+            get => _groupno; 
+            set
+            {
+                while (!CheckGroupNo(value)) // value gonderirik chech metoduna o da ki true gelse false olur, ili je naoborot
+                {
+                    Console.WriteLine("Grupun nomresini duz yazin.");
+                    value = Console.ReadLine();
+                }
+                _groupno = value;
+            }
+        }
+        public static bool CheckFullname(string fullname) => fullname.Split(' ').Length >= 2;
+        
+        public static bool CheckGroupNo(string GroupNo)
+        {
+            if (!(GroupNo.Length == 4 && char.IsUpper(GroupNo[0]) && char.IsDigit(GroupNo[1]) && char.IsDigit(GroupNo[2]) && char.IsDigit(GroupNo[3]))) 
+            {
+                return false;
+            }
+            return true;  // for loop da yazmag olardi i=1 bashlasaydi ve groupno[i] ++ olardi groupno.Lenght-e geder, onda coxlu return olacagdi.
+
+                
+        }
+        public Student(string name, string surname, byte age, string groupno)
+        {
+            FullName = $"{name} {surname}";
+            Age = age;
+            GroupNo = groupno;
+        }
+    }
+}
---
+++ Task1_never_say_never/Task1_never_say_never/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Task1_never_say_never.Models;
+
+namespace Task1_never_say_never
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool check = true;
+            //while (check)
+            //{
+                Console.WriteLine("Salam.\nAdiniz:");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Soyadiniz:");
+                string surname = Console.ReadLine();
+
+                Console.WriteLine("Zehmet olmasas yashinizi qeyd edin:");
+                string age = Console.ReadLine();
+                byte parsedage = byte.Parse(age);
+
+                Console.WriteLine("Grupunuzun adini qeyd edin, amma duz yazin:");
+                string groupno = Console.ReadLine();
+
+                Student student = new Student(name, surname, parsedage, groupno);
+
+            Console.WriteLine($"Adiniz ve Soyadiniz: {student.FullName}\nYashiniz {student.Age}\nOxudugunuz grupun nomresi: {student.GroupNo}\n___________________");
+                //ConsoleKeyInfo y = Console.ReadKey();
+
+                //if (y.Key.ToString() == "y")
+                //{
+                //    Console.WriteLine("Onda sagolun");
+                //    check = false;
+                //    break;
+                //}
+            //}
+            //Student student = new Student(name, surname, parsedage, groupno);
+            //Console.WriteLine($"Adiniz ve Soyadiniz: {student.FullName}\nYashiniz {student.Age}\nOxudugunuz grupun nomresi: {student.GroupNo}");
+        }
+    }
+}
